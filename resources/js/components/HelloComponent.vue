013b7c0badcb8cf43af3f35ab6be05cf3fc615f8
terism7 <terism7@gmail.com>
2018-09-12 21:59:16 +0700

    Initial commit

---
+++ resources/js/components/HelloComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card card-default">
+                    <div class="card-header">{{pageName}}</div>
+                    <div class="card-body">
+                        Hello World
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                pageName: 'CRUD VueJS Have Fun!'
+            }
+        },
+        mounted() {
+            console.log('Hello World Mounted.')
+        }
+    }
+</script>
